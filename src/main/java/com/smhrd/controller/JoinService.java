16eb59daa51a039443e622a1452198537b527592
smhrd <smhrd@59.0.129.202>
2023-01-09 11:32:21 +0900

    0109민재->화진

---
+++ src/main/java/com/smhrd/controller/JoinService.java
@@ -0,0 +1,43 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.smhrd.model.tb_user;
+import com.smhrd.model.tb_userDAO;
+
+
+public class JoinService extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+
+   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      request.setCharacterEncoding("utf-8");
+      
+      String user_id = request.getParameter("user_id");
+      String user_pw = request.getParameter("user_pw");
+      String user_name = request.getParameter("user_name");
+      String user_nick = request.getParameter("user_nick");
+      String user_email = request.getParameter("user_email");
+      String user_aog = request.getParameter("user_aog");
+      String gender = request.getParameter("gender");
+      String yob = request.getParameter("yob");
+      String phone = request.getParameter("phone");
+
+      
+      tb_user user = new tb_user(user_id, user_pw, user_name, user_nick, user_email, user_aog, gender, yob, phone);
+      
+      tb_userDAO dao = new tb_userDAO();
+      int res = dao.join(user);
+      
+      if(res > 0) { //삽입성공
+         response.sendRedirect("login.html");
+      }else {//삽입실패
+         response.sendRedirect("join.html");
+      }
+   }
+
+}
---
+++ src/main/java/com/smhrd/model/tb_user.java
@@ -0,0 +1,171 @@
+package com.smhrd.model;
+
+public class tb_user {
+   private String user_id;
+   private String user_pw;
+   private String user_name;
+   private String user_nick;
+   private String user_email;
+   private String user_aog;
+   private String gender;
+   private String yob;
+   private String phone;
+
+   
+   
+
+   public tb_user(String user_id, String user_pw, String user_name, String user_nick, String user_email, String user_aog,
+		String gender, String yob, String phone) {
+	super();
+	this.user_id = user_id;
+	this.user_pw = user_pw;
+	this.user_name = user_name;
+	this.user_nick = user_nick;
+	this.user_email = user_email;
+	this.user_aog = user_aog;
+	this.gender = gender;
+	this.yob = yob;
+	this.phone = phone;
+}
+
+
+
+
+public tb_user() {
+	super();
+}
+
+
+
+
+public String getUser_id() {
+	return user_id;
+}
+
+
+
+
+public void setUser_id(String user_id) {
+	this.user_id = user_id;
+}
+
+
+
+
+public String getUser_pw() {
+	return user_pw;
+}
+
+
+
+
+public void setUser_pw(String user_pw) {
+	this.user_pw = user_pw;
+}
+
+
+
+
+public String getUser_name() {
+	return user_name;
+}
+
+
+
+
+public void setUser_name(String user_name) {
+	this.user_name = user_name;
+}
+
+
+
+
+public String getUser_nick() {
+	return user_nick;
+}
+
+
+
+
+public void setUser_nick(String user_nick) {
+	this.user_nick = user_nick;
+}
+
+
+
+
+public String getUser_email() {
+	return user_email;
+}
+
+
+
+
+public void setUser_email(String user_email) {
+	this.user_email = user_email;
+}
+
+
+
+
+public String getUser_aog() {
+	return user_aog;
+}
+
+
+
+
+public void setUser_aog(String user_aog) {
+	this.user_aog = user_aog;
+}
+
+
+
+
+public String getGender() {
+	return gender;
+}
+
+
+
+
+public void setGender(String gender) {
+	this.gender = gender;
+}
+
+
+
+
+public String getYob() {
+	return yob;
+}
+
+
+
+
+public void setYob(String yob) {
+	this.yob = yob;
+}
+
+
+
+
+public String getPhone() {
+	return phone;
+}
+
+
+
+
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+
+
+
+
+
+
+	
+	
+}
